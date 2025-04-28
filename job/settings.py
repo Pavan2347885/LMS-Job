580bfe7c92c6c20e632b6ad492befe6a62b231a4
Pavan <pavan10192003@gmail.com>
2025-04-28 15:49:03 +0530

    444

--- job/settings.py
+++ job/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 import os
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-ALLOWED_HOSTS = ['lms-job.onrender.com']
+ALLOWED_HOSTS = []
+# 'lms-job.onrender.com'
 
 import os
 
